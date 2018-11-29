22fdd124cacd6da68374d7789d74a782e7a09576
Emerson Iraheta <ceiraheta87@gmail.com>
2018-11-29 16:27:24 -0600

    creating models for s3

--- posgrados/services/models.py
+++ posgrados/services/models.py
@@ -4,11 +4,6 @@ from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 #
-
-
-
-
-
 class Noticia (models.Model) :
     emcabezado = models.CharField(max_length=50)
     cuerpo = models.CharField(max_length=100)
@@ -101,7 +96,7 @@ class Pasos(models.Model):
     def __str__(self):
         return(self.nombre)
 
-class Cita (models.Model):
+class Cita(models.Model):
     id_cita=models.AutoField(primary_key=True)
     id_user_para = models.ForeignKey(User, models.SET_NULL, blank=True, null=True, )
     id_user_con = models.ForeignKey(User, models.SET_NULL, blank=True, null=True, related_name='user2')
@@ -114,3 +109,61 @@ class Cita (models.Model):
     nombre_con=models.CharField(max_length=150,blank=True,)
     cancelado=models.BooleanField()
     dia_completo=models.BooleanField()
+
+    def __str__(self):
+        return self.titulo
+
+class Programa(models.Model):
+    id_programa=models.AutoField(primary_key=True)
+    codigo=models.CharField(max_length=10)
+    nombre=models.CharField(max_length=150)
+    descripcion=models.CharField(max_length=1024, blank=True, null=True)
+    totalUV=models.IntegerField()
+    plan_estudio=models.IntegerField()
+    duracion_ciclo=models.IntegerField()
+    duracion_anio=models.IntegerField()
+    titulo=models.CharField(max_length=100)
+    total_asignaturas=models.IntegerField()
+    nota_minima=models.DecimalField(max_digits=4,decimal_places=2)
+    cum_minimo=models.DecimalField(max_digits=4,decimal_places=2)
+    caracteristicas=models.CharField(max_length=1024, blank=True, null=True)
+
+    def __str__(self):
+        return self.codigo
+
+class ciclo (models.Model):
+    id_ciclo=models.AutoField(primary_key=True)
+    numero=models.IntegerField()
+    anio=models.IntegerField()
+    
+    def __str__(self):
+        return (str(self.id_ciclo))
+
+class Materia(models.Model):
+    id_materia=models.AutoField(primary_key=True)
+    id_user_para = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
+    id_materia_pre= models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+    id_ciclo= models.ForeignKey(ciclo, models.SET_NULL, blank=True, null=True)
+    codigo=models.CharField(max_length=10)
+    nombre=models.CharField(max_length=150)
+    correlativo=models.IntegerField()
+    unidad_valorativa=models.IntegerField()
+
+    def __str__(self):
+        return (self.codigo)
+
+class aula(models.Model):
+    id_aula=models.AutoField(primary_key=True)
+    codigo=models.CharField(max_length=10)
+    ubicacion=models.CharField(max_length=100)
+
+    def __str__(self):
+        return(self.codigo)    
+
+class horario(models.Model):
+    id_horario=models.AutoField(primary_key=True)
+    hora_inicio=models.TimeField()
+    hora_fin=models.TimeField()
+    activo=models.BooleanField()
+    def __str__(self):
+        return(self.codigo)
